1dd93ec02ba0a31497e43fb091dd0057bba2f463
Richar Irasabal Belles <irasabal.richy@gmail.com>
2022-09-22 01:40:41 -0400

    se agrego el formulario de Log In

---
+++ frontend-salb/src/componentes/Fotos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Fotos = () => {
+    return (
+        <div>
+            <p>Cuadro de fotos del juego</p>
+        </div>
+    );
+}
+
+export default Fotos;
+
--- frontend-salb/src/componentes/barraNavegacion.js
+++ frontend-salb/src/componentes/barraNavegacion.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../css/styleNavBar.css';
 import logo from '../imagenes/logoLigaBasket1.png';
+import {Link} from 'react-router-dom';
+
+
 const BarraNavegacion = () => {
     return(
         <div className='contenedorPrincipalNavBar'>
@@ -9,8 +12,8 @@ const BarraNavegacion = () => {
             </div>
             <div className='inicarSesionRegistrarse'>
                 <label className='nombreLigaBasquet'>LIGA MAXI BASQUET</label>
-                <a className='registrarse'>REGISTRARSE</a>
-                <a className='iniciarSesion'>INICIAR SESIÓN</a>
+                <Link className='registrarse' to="/register">REGISTRARSE</Link>
+                <Link className='iniciarSesion' to="/login">INICIAR SESIÓN</Link>
             </div>
             <nav class="navbar navbar-expand-lg bg-light fondoNavBar tamañoFondo">
                 <div class="container-fluid tamañoFondo">
@@ -22,12 +25,12 @@ const BarraNavegacion = () => {
                     <div class="collapse navbar-collapse tamañoFondo" id="navbarNavAltMarkup">
                         <div class="navbar-nav menuOpciones tamañoFondo">
                             <a class="nav-link active colorLetra contenedorLetrasNavBar" aria-current="page" href="#">ESTADISTICAS</a>
-                            <a class="nav-link colorLetra contenedorLetrasNavBar" href="#">FOTOS</a>
-                            <a class="nav-link colorLetra contenedorLetrasNavBar" href="#">EQUIPOS</a>
-                            <a class="nav-link colorLetra contenedorLetrasNavBar" href="#">JUEGOS</a>
-                            <a class="nav-link colorLetra contenedorLetrasNavBar" href="#">NOTICIAS</a>
-                            <a class="nav-link colorLetra contenedorLetrasNavBar" href="#">EN VIVO</a>
-                            <a class="nav-link colorLetra contenedorLetrasNavBar" href="#">SOBRE NOSOTROS</a>
+                            <Link class="nav-link colorLetra contenedorLetrasNavBar" to="/fotos">FOTOS</Link>
+                            <Link class="nav-link colorLetra contenedorLetrasNavBar" to="/equipos">EQUIPOS</Link>
+                            <Link class="nav-link colorLetra contenedorLetrasNavBar" to="/juegos">JUEGOS</Link>
+                            <Link class="nav-link colorLetra contenedorLetrasNavBar" to="/noticias">NOTICIAS</Link>
+                            <Link class="nav-link colorLetra contenedorLetrasNavBar" to="/en-vivo">EN VIVO</Link>
+                            <Link class="nav-link colorLetra contenedorLetrasNavBar" to="/sobre-nosotros">SOBRE NOSOTROS</Link>
                         </div>
                     </div>
                 </div>
