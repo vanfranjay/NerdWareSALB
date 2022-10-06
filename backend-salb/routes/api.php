bb3f2c2cd09ac039951074af4668bb9edb7c7370
Fabsland1a <fabybelencespedest@gmail.com>
2022-10-05 20:45:09 -0400

    API

--- backend-salb/routes/api.php
+++ backend-salb/routes/api.php
@@ -9,12 +9,8 @@ use App\Http\Controllers\JugadorController;
 use App\Http\Controllers\PersonaController;
 use App\Http\Controllers\BoletaController;
 use App\Http\Controllers\EquipoController;
-<<<<<<< HEAD
-use App\Http\Controllers\DelegadoController;
-use App\Http\Controllers\DelBolController;
-=======
-
->>>>>>> 27e605b75b1f33ec339945f5ff847fb8855479a5
+//use App\Http\Controllers\DelegadoController;
+//use App\Http\Controllers\DelBolController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -30,15 +26,11 @@ use App\Http\Controllers\DelBolController;
   //  return $request->user();
 //});
 Route::resource('boletas', BoletaController::class);
-<<<<<<< HEAD
 Route::resource('equipo', EquipoController::class);
 Route::resource('delegados', DelegadoController::class);
-Route::resource('delbos', DelBolController::class);
-=======
+//Route::resource('delbos', DelBolController::class);
 Route::resource('personas', PersonaController::class);
-Route::resource('delegados', DelegadoController::class);
+//Route::resource('delegados', DelegadoController::class);
 Route::resource('delbos', DelBolController::class);
-Route::resource('equipos', EquipoController::class);
+//Route::resource('equipos', EquipoController::class);
 Route::resource('jugadores', JugadorController::class);
-
->>>>>>> 27e605b75b1f33ec339945f5ff847fb8855479a5
