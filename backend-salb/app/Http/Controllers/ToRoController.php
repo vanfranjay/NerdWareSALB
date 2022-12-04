db73483eb65afb3a1275371d5df1ab47264653b1
fabrizio.oropeza <201910093@est.umss.edu>
2022-12-03 23:27:17 -0400

    Mostrar por fechas los partidos

--- backend-salb/app/Http/Controllers/ToRoController.php
+++ backend-salb/app/Http/Controllers/ToRoController.php
@@ -33,14 +33,17 @@ class ToRoController extends Controller
        }
        $aux = $año ."-". $mes ."-". $dia1;
       // $hoy1 = Date("$año", "$mes", "$dia1");
-       $hoy1= Date($aux);
-       $hora1 = Date("24:00:00");
+      // $hoy1= Date($aux);
+      $hoy1 = DB::table('torneos')->select('torneos.Fecha_Fin_Torneo')->value('torneos.Fecha_Fin_Torneo');
+      $hora1 = Date("24:00:00");
       // return $hora; 
         return DB::table('torneos')
         ->join('rol_partidos', 'torneos.id', '=', 'rol_partidos.Cod_Torneo')
         ->select('rol_partidos.*', 'torneos.Nombre_Torneo')
         ->whereBetween('Fecha', [$hoy, $hoy1])
         //->whereBetween('Hora', [$hora, $hora1])
+        ->orderBy('Fecha', 'asc')
+        ->orderBy('Hora', 'asc')
         ->get();   //
     }
 
