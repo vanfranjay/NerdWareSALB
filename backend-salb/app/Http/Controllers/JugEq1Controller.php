ebd967bcba7957ba90dfc8c759463c1910fb1bf3
fabrizio.oropeza <201910093@est.umss.edu>
2022-12-09 23:42:09 -0400

    Arreglo de controlodares, en los gets

--- backend-salb/app/Http/Controllers/JugEq1Controller.php
+++ backend-salb/app/Http/Controllers/JugEq1Controller.php
@@ -71,7 +71,9 @@ class JugEq1Controller extends Controller
     {
         return DB::table('jugadores')
         ->join('equipos', 'equipos.id', '=', 'jugadores.Cod_Equipo')
-        ->join('categorias','categorias.id','=', 'jugadores.Cod_Categoria')
+        ->join('delegados', 'delegados.id', '=' ,'equipos.Cod_Delegado')
+        ->join('torneos', 'torneos.id', '=', 'delegados.Cod_Torneo')
+        ->join('categorias','categorias.Cod_Torneo','=', 'torneos.id')
         ->select('jugadores.*', 'equipos.Nombre_Equipo', 'categorias.Categoria')
         ->where('jugadores.Cod_Equipo',$id)
         ->get();
